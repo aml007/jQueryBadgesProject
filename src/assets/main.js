5645278dbfc07b2ed62dee80c6c6762ffb95848b
aml007 <albertomartinezlope@gmail.com>
2017-09-05 22:45:33 +0200

    Completed tasks.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/aml007.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var courses = response.courses.completed;
 
-  // your code will go here
+      $.each(courses, function(index, course) {
+        var title = $('<h3>').text(course.title);
+        var image = $('<img>').attr('src', course.badge);
+        var link = $('<a>').attr({href: course.url, target: '_blank'})
+          .addClass('btn btn-primary').text('See Course');
 
+        var course = $('<div>').addClass('course')
+          .append(title)
+          .append(image)
+          .append(link);
+
+        $('#badges').append(course);
+      });
+    }
+  });
 });
